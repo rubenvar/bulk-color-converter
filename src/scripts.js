471a96b09def1a743ce56ee1f41d719fe3d4f6d9
Rubén <hey@rubenvara.io>
2021-03-22 12:59:24 +0100

    add hexa color feature and cleanup code

--- src/scripts.js
+++ src/scripts.js
@@ -1,40 +1,54 @@
 // formulas from https://css-tricks.com/converting-color-spaces-in-javascript/
 const userInput = document.getElementById('hex');
-const button = document.getElementById('convert');
+const convert = document.getElementById('convert');
 const result = document.getElementById('result');
 const reset = document.getElementById('reset');
 
-const regex = /^(#)?[a-fA-F0-9]{3}$|^(#)?[a-fA-F0-9]{6}$/;
+const regex = /^(#)?[a-fA-F0-9]{3}$|^(#)?[a-fA-F0-9]{4}$|^(#)?[a-fA-F0-9]{6}$|^(#)?[a-fA-F0-9]{8}$/;
 
 function calculateHSL(inp) {
   // first check hex format is right, else return
   const isHex = regex.test(inp);
-  if (!isHex) return console.error(`${inp} not valid (╯°□°）╯︵ ┻━┻`);
+  if (!isHex) throw new Error(`${inp} not valid (╯°□°）╯︵ ┻━┻`);
   // strip the #
   const hex = inp.replace('#', '');
+  // get input length
+  const hexLength = hex.length;
+
+  // check if it has alpha channel
+  const hasAlpha = hexLength === 4 || hexLength === 8;
+
   // hex to rgb (add '0x' in front and convert to Int to get decimal from haxdecimals 😉)
   let r;
   let g;
   let b;
-  // check if hex is 3 or 6 length
-  if (hex.length === 6) {
+  let a;
+
+  // check which length
+  if (hexLength === 6 || hexLength === 8) {
     r = +`0x${hex[0]}${hex[1]}`;
     g = +`0x${hex[2]}${hex[3]}`;
     b = +`0x${hex[4]}${hex[5]}`;
-  } else if (hex.length === 3) {
+    if (hasAlpha) a = +`0x${hex[6]}${hex[7]}`;
+  } else if (hexLength === 3 || hexLength === 4) {
     r = +`0x${hex[0]}${hex[0]}`;
     g = +`0x${hex[1]}${hex[1]}`;
     b = +`0x${hex[2]}${hex[2]}`;
-  } else return console.log(`${inp} not valid (╯°□°）╯︵ ┻━┻`);
+    if (hasAlpha) a = +`0x${hex[3]}${hex[3]}`;
+  } else throw new Error(`${inp} not valid (╯°□°）╯︵ ┻━┻`);
+
   // convert rgb to hsl:
   // 1. divide by 255
   r /= 255;
   g /= 255;
   b /= 255;
+  if (hasAlpha) a = (a / 255).toFixed(2);
+
   // 2. get max, min, and difference between max and min
   const min = Math.min(r, g, b);
   const max = Math.max(r, g, b);
   const delta = max - min;
+
   // 3. calculate Hue
   let h;
   // no difference between channels
@@ -47,32 +61,37 @@ function calculateHSL(inp) {
   h = Math.round(h * 60);
   // only positives under 360
   if (h < 0) h += 360;
-  // calculate Lightness
+
+  // 4. calculate Lightness
   let l = (max + min) / 2;
-  // calculate Saturation
+
+  // 5. calculate Saturation
   let s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
   // L and S to hundreds (for percent)
   l = +(l * 100).toFixed(0);
   s = +(s * 100).toFixed(0);
-  // out hsl formatted for css
+
+  // 🚀 out hsl (or hsla) formatted for css
+  if (hasAlpha) return `hsla(${h}, ${s}%, ${l}%, ${a})`;
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-button.addEventListener(
-  'click',
-  () =>
-    // asasa
-    (result.value = userInput.value
-      // strip commas, spaces, newlines, empty, etc.
-      .split(',')
-      .map(h => h.replace(/ /g, ''))
-      .map(h => h.replace(/\n/g, ''))
-      .filter(h => h !== '')
-      // get hsl
-      .map(h => calculateHSL(h))
-      // into string with newlines
-      .join('\n'))
-);
+convert.addEventListener('click', () => {
+  const output = userInput.value
+    // strip commas, spaces, newlines, empty, etc.
+    .split(',')
+    .map((h) => h.replace(/ /g, ''))
+    .map((h) => h.replace(/\n/g, ''))
+    .filter((h) => h !== '')
+    // get hsl
+    .map((h) => calculateHSL(h))
+    // into string with newlines
+    .join('\n');
+
+  result.value = output;
+  result.style.borderColor = output;
+});
 
 reset.addEventListener('click', () => {
   userInput.value = '';
